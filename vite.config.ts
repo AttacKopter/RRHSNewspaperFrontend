cac0df4a92f5d92dc1910d1fd42c72c0ddbcc105
AttacKopter <maximus.rzepka@gmail.com>
2024-04-29 16:25:21 -0500

    ssl key reverted

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from "node:url";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+console.log(process.env.VITE_SSL_KEY)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,8 +14,8 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 4000,
     https: {
-      key: '~/privkey.pem',
-      cert: '~/fullchain.pem',
+      key: process.env.VITE_SSL_KEY,
+      cert: process.env.VITE_SSL_CERT,
     },
     hmr: {
       overlay: false,
